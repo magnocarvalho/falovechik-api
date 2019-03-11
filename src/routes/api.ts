64a0aeda0595510b5f3c144f6664e3bf6b5cb224
Magno Carvalho <magnocarv@hotmail.com>
2019-03-10 21:40:56 -0300

    reformulando api do projeto

---
+++ src/routes/api.ts
@@ -0,0 +1,19 @@
+import * as express from 'express';
+import UsuarioCtrl from '../controllers/UsuarioCtrl';
+import { photosModel } from '../model/definitions/Photo';
+import PhotoCtlr from '../controllers/photoCtlr';
+
+var mcache = require('memory-cache');
+
+var router = express.Router();
+
+router.post('/salvarUsuario',UsuarioCtrl.create);
+router.post('/loginUser', UsuarioCtrl.login);
+
+router.post('/salvarFotos' ,PhotoCtlr.putPhotos);
+router.get('/carregarAlbuns/:id', PhotoCtlr.buscarAlbuns);
+router.get('/carregarFotos/:id', PhotoCtlr.buscarAlbum);
+router.post('/apagarFoto', PhotoCtlr.deletarFoto);
+router.put('/addFotos',PhotoCtlr.addFotos);
+
+export = router;
