97751b9c0c1f9a2c34c76eae5dba423cac1f9fb1
Magno Carvalho <magnocarv@hotmail.com>
2019-04-03 20:58:42 -0300

    mudanças na api

--- src/routes/api.ts
+++ src/routes/api.ts
@@ -1,6 +1,5 @@
 import * as express from 'express';
 import UsuarioCtrl from '../controllers/UsuarioCtrl';
-import { photosModel } from '../model/definitions/Photo';
 import PhotoCtlr from '../controllers/photoCtlr';
 
 
